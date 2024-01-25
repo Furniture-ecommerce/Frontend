ef6fe1fec76dc0c078cc04f577e6b1f4f2cbb838
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2024-01-25 12:19:06 +0700

    create banner grid on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,69 @@
-import { Carousel } from "@components/Carousel";
+import { BannerCarousel } from "@components/BannerCarousel";
+import { ProductCarousel } from "@components/ProductCarousel";
 
 const Home = () => {
   return (
-    <div>
-      <div className="container mx-auto px-10">
-        <Carousel />
+    <div className="mx-auto max-w-screen-xl">
+      <div>
+        <BannerCarousel />
       </div>
+      <div class="py-10 place-content-center flex items-center justify-center">
+        <div class="flex-initial w-full">
+          <div className="text-7xl">
+            <b className="text-black">
+              Simply Unique<span className="text-gray-600">/</span> <br />
+              Simply Better<span className="text-gray-600">.</span>
+            </b>
+          </div>
+        </div>
+        <div class="flex-initial w-full ">
+          <div>
+            <span className="text-black">
+              <b>3legant </b>
+            </span>
+            is a gift & decorations store based in HCMC, Vietnam. Est since
+            2019.
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-rows-2 grid-flow-col gap-3">
+        <div class="row-span-2 col-span-2">
+          <img
+            src="/assets/images/elements/loveseat.png"
+            class="w-full h-full object-cover"
+            alt="loveseat"
+          />
+        </div>
+        <div class="col-span-2">
+          <img
+            src="/assets/images/elements/small-cupboards.png"
+            class="w-full h-full object-cover"
+            alt="small-cupboards"
+          />
+        </div>
+        <div class="col-span-2">
+          <img
+            src="/assets/images/elements/toaster.png"
+            class="w-full h-full object-cover"
+            alt="toaster"
+          />
+        </div>
+      </div>
+      <div class="py-10 flex">
+        <div class="flex-initial w-full">
+          <div className="text-4xl">
+            <b className="text-black">
+              New
+              <br />
+              Arrivals
+            </b>
+          </div>
+        </div>
+        <div class="flex-initial w-full text-black mt-auto text-right">
+          More Product
+        </div>
+      </div>
+      <ProductCarousel/>
     </div>
   );
 };
---
+++ components/ProductCarousel.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    image: "/assets/images/sofa-loveseat.png",
+    description: "Description of Product 1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    image: "/assets/images/small-lamp.png",
+    description: "Description of Product 2",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    image: "/assets/images/lamp.png",
+    description: "Description of Product 3",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    image: "/assets/images/bask.png",
+    description: "Description of Product 4",
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    image: "/assets/images/toaster.png",
+    description: "Description of Product 5",
+  },
+  {
+    id: 6,
+    name: "Product 1",
+    image: "/assets/images/sofa-loveseat.png",
+    description: "Description of Product 1",
+  },
+  {
+    id: 7,
+    name: "Product 2",
+    image: "/assets/images/small-lamp.png",
+    description: "Description of Product 2",
+  },
+  {
+    id: 8,
+    name: "Product 3",
+    image: "/assets/images/lamp.png",
+    description: "Description of Product 3",
+  },
+  {
+    id: 9,
+    name: "Product 4",
+    image: "/assets/images/bask.png",
+    description: "Description of Product 4",
+  },
+  {
+    id: 10,
+    name: "Product 5",
+    image: "/assets/images/toaster.png",
+    description: "Description of Product 5",
+  },
+];
+
+export const ProductCarousel = () => {
+  return (
+    <div className="max-w-md p-4 overflow-hidden relative">
+    </div>
+  );
+};
